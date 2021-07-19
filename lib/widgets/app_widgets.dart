24489241aa37e8bde46bf907c47c452cab37ae1d
DESKTOP-9R483HM\user <theanunr@gmail.com>
2021-07-19 13:22:51 +0700

    add loading widget.

--- lib/widgets/app_widgets.dart
+++ lib/widgets/app_widgets.dart
@@ -2,3 +2,4 @@ export 'card_widget.dart';
 export 'button_widget.dart';
 export 'circle_press_widget.dart';
 export 'safe_area_widget.dart';
+export 'loading_widget.dart';
---
+++ lib/widgets/loading_widget.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import '../constants/app_constants.dart';
+
+// ignore: must_be_immutable
+class LoadingWidget extends StatelessWidget {
+  var color;
+  LoadingWidget({this.color});
+
+  @override
+  Widget build(BuildContext context) {
+
+    color = color == null ? Colors.black45 : color;
+
+    return Stack(
+      children: [
+        //Blur Background
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 0.0, sigmaY: 0.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Colors.black12.withOpacity(0.05),
+          ),
+        ),
+
+        // CircleProgress()
+        Padding(
+          padding: EdgeInsets.all(LatePaddingSize.semiBig),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Container(
+                    width: LatePaddingSize.big,
+                    height: LatePaddingSize.big,
+                    child: ClipOval(
+                      child: Container(
+                        color: Colors.white,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(color)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
